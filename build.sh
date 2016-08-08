13e4308c419b45aeb4d0d489a80b3648530986ea
Fairy Coder <fairycoder@gmail.com>
2016-08-08 23:07:54 +0300

    Add build script for mac/linux environment

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+dotnet restore
+
+dotnet test ./test/SharpBlueprint.Core.Tests -c Release
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
+
+dotnet pack ./src/SharpBlueprint.Core -c Release -o ./artifacts --version-suffix=$revision 
