0d2a41b363232a993ef5ee25b8a6ee0fd4a6a60c
Fairy Coder <fairycoder@gmail.com>
2016-08-09 21:12:09 +0300

    Fix compilation: remove conditional compilation symbols

--- test/SharpBlueprint.Core.Tests/SimpleHttpClientTests.cs
+++ test/SharpBlueprint.Core.Tests/SimpleHttpClientTests.cs
@@ -1,29 +1,10 @@
 ﻿using System;
-#if NET35
-using NUnit.Framework;
-#else
 using Xunit;
-#endif
 
 namespace SharpBlueprint.Core.Tests
 {
-#if NET35
-    [TestFixture]
-#endif
     public class SimpleHttpClientTests
     {
-#if NET35
-        [Test]
-        public void GetDotNetCountTest()
-        {
-            var client = new SimpleHttpClient();
-            var result = client.GetDotNetCount();
-
-            Console.WriteLine(result);
-
-            Assert.IsTrue(!string.IsNullOrEmpty(result));
-        }
-#else
         [Fact]
         public void GetDotNetCountAsyncTest()
         {
@@ -36,6 +17,5 @@ namespace SharpBlueprint.Core.Tests
 
             Assert.True(!string.IsNullOrEmpty(result));
         }
-#endif
     }
 }
