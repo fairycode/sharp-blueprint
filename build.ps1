27fb586fbc8815b9f2d3844fc909e716679f837e
Fairy Coder <fairycoder@gmail.com>
2016-08-09 19:17:11 +0300

    Try to fix issue with nuget restore

--- build.ps1
+++ build.ps1
@@ -1,11 +1,15 @@
 cls
 
+#if ($LastExitCode -ne 0) { Write-Host "Error code:" $LastExitCode }
+
 # '[p]sake' is the same as 'psake' but $Error is not polluted
 remove-module [p]sake
 
 # find psake's path
-$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | Select -Last 1
  
+#if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
 Import-Module $psakeModule
 
 # you can put arguments to task in multiple lines using `
