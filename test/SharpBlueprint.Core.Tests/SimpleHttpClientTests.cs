1dda71f7f8fd47f21140f3d9e3c05fe84aeef85d
Fairy Coder <fairycoder@gmail.com>
2016-08-09 22:52:05 +0300

    Come back to conditional compilation

--- test/SharpBlueprint.Core.Tests/SimpleHttpClientTests.cs
+++ test/SharpBlueprint.Core.Tests/SimpleHttpClientTests.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+#if NET35
+using NUnit.Framework;
+#else
 using Xunit;
+#endif
 
 namespace SharpBlueprint.Core.Tests
 {
+#if NET35
+    [TestFixture]
+#endif
     public class SimpleHttpClientTests
     {
+#if NET35
+        [Test]
+        public void GetDotNetCountTest()
+        {
+            var client = new SimpleHttpClient();
+            var result = client.GetDotNetCount();
+
+            Console.WriteLine(result);
+
+            Assert.IsTrue(!string.IsNullOrEmpty(result));
+        }
+#else
         [Fact]
         public void GetDotNetCountAsyncTest()
         {
@@ -17,5 +36,6 @@ namespace SharpBlueprint.Core.Tests
 
             Assert.True(!string.IsNullOrEmpty(result));
         }
+#endif
     }
 }
